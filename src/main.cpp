80ceac3499cfee72ebf1ffb85ad1f5b72bc8100c
bmseibert <bmseibert@wpi.edu>
2020-06-15 21:37:45 -0400

    changing startup sequence

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,11 @@ int main()
   int val = gpioInitialise();
   std::cout << "Initailization val: " << val << std::endl;
   gpioSetMode(17, PI_OUTPUT);
+  gpioSetPWMfrequency(17, 30000);
   gpioPWM(17, 0);
   gpioPWM(17, 255);
   gpioPWM(17, 50);
+  std::cout << "PWM set up done" << std::endl;
   // FlightCont mainController;
   // std::cout << "Pi number is: " << mainController.GetPi() << std::endl;
   // mainController.frontr->SetSpeed(25);
