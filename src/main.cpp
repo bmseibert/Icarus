add948790cd7fc0fd2c84ab7ef04fae390769f3a
bmseibert <bmseibert@wpi.edu>
2020-06-28 17:11:52 -0400

    changing startup sequence

--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,31 @@ int main()
   std::cout << "Initailization val: " << val << std::endl;
   bool calibrate = false;
   char enter[10];
-  gpioServo(18, 0);
-  gpioSetMode(18, PI_OUTPUT);
+  gpioServo(22, 0);
+  gpioSetMode(22, PI_OUTPUT);
   if (calibrate){
-    gpioServo(18, 0);
-    gpioServo(18, 2000); //set it to max value
+    gpioServo(22, 0);
+    gpioServo(22, 2000); //set it to max value
     std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-    gpioServo(18, 1000); // set it to the min value
+    gpioServo(22, 1000); // set it to the min value
     std::this_thread::sleep_for(std::chrono::milliseconds(12000));
-    gpioServo(18, 0); // set it to 0
+    gpioServo(22, 0); // set it to 0
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    gpioServo(18, 1000); // Now it is armed
+    gpioServo(22, 1000); // Now it is armed
     std::cout << "PWM set up done" << std::endl;
   }
   else{
-    gpioServo(18, 0); //set it to 0
+    gpioServo(22, 0); //set it to 0
     std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-    gpioServo(18, 2000); // set it to the min value
+    gpioServo(22, 2000); // set it to the min value
     std::this_thread::sleep_for(std::chrono::milliseconds(4000));
-    gpioServo(18, 1000); // set it to 0
+    gpioServo(22, 1000); // set it to 0
     std::this_thread::sleep_for(std::chrono::milliseconds(4000));
   }
   std::cout << "Running motor at quater speed" << std::endl;
   
   std::cout << "In Loop" << std::endl;
-  gpioServo(18, 1200);
+  gpioServo(22, 1200);
   
   // FlightCont mainController;
   // std::cout << "Pi number is: " << mainController.GetPi() << std::endl;
